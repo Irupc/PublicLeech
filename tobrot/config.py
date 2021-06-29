a5bc737e1f98ea513c410c41982a3959126eaec8
Irupc <68991705+Irupc@users.noreply.github.com>
2021-06-29 16:22:15 +0530

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,7 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+  TG_BOT_TOKEN = "1732182362:AAGDMcnIYXBexwOeIn-rsE124pT1ptuQc70"
+  APP_ID = 3206260
+  API_HASH = "fbc8918eed3b68ccfd80283cf53db785"
+  AUTH_CHANNEL = -1001286992398
